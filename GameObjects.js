eec9094dd1f7a978b187b5524f53012961e9264e
Bulatov Aidamir <bulatov_a@utkonos.ru>
2021-07-18 02:05:45 +0300

    Немножко рефакторинга и пищи для мозгов. Я совсем немножко набросал, дальше ты разгребай и дорабатывай. Давай, я в тебя верю.

---
+++ GameObjects.js
@@ -0,0 +1,40 @@
+class Figure {
+    team_map = ['black', 'white'];
+    coor_x = ["A","B","C","D","E","F","G","H"];
+    coor_y =["8","7","6","5","4","3","2","1"];
+
+    constructor(location, team) {
+        if (this.team_map.indexOf(team) === -1) return 'ERROR INVALID TEAM PIDOR: only black or white allowed';
+        this.team = team;
+        this.location = location;
+    }
+
+    get_coordinates() {
+        let coor_arr = this.location.split('');
+        if (coor_arr[0].indexOf(this.coor_x) === -1 || coor_arr[0].indexOf(this.coor_x) === -1) return 'ERROR INVALID LOCATION PIDOR: how did it get here?!';
+
+        return {
+            "x": coor_arr[0],
+            "y": coor_arr[1]
+        }
+    }
+}
+
+class Pawn extends Figure {
+    is_first_move = true;
+
+    background_map = {
+        'black': '/img/black_pawn.png',
+        'white': '/img/white_pawn.png'
+    };
+
+    constructor(location, team) {
+        super(location, team);
+        this.background = this.background_map[this.team];
+    }
+
+    get_allowed_moves() {
+        let coordinates = this.get_coordinates();
+        //todo дописать логику, надо чтобы функция возвращала все возможные ходы из текущей позиции которую я любезно записал в coordinates
+    }
+}
--- Script-5.js
+++ Script-5.js
@@ -1,110 +1,59 @@
+
+
 function Chess(){
 	var mainBlock = document.querySelector(".main-block");
-	var block ;
-	var flag= true ;
-	var arr= [' ',"A","B","C","D","E","F","G","H"," "];
-	var arr2=[" ","8","7","6","5","4","3","2","1"," "]
-	var sas =9;
-	for (var i = 0; i<10; i++) {
-		for (var j = 0; j < 10; j++) {
-			if(j==0)flag=!flag;
-			block = document.createElement("block");
-			if(flag) block.className = "block black";
-			else block.className = "block white";
-			block.dataset.adress=arr[j]+arr2[i];
-			//console.log(block.dataset.adress);
+	var block, className;
+	var flag = true;
+	var arr = [" ","A","B","C","D","E","F","G","H"," "];
+	var arr2 =[" ","8","7","6","5","4","3","2","1"," "]
+
+	for (let i = 0; i < 10; i++) {
+		for (let j = 0; j < 10; j++) {
+			block = document.createElement("div");
+			className = "block";
 			mainBlock.appendChild(block);
-			flag=!flag;
-			var allblock = document.querySelectorAll (".block");
-			allblock[i].className="block text";
-			allblock[i].innerText=arr[i];
+
+			if (i === 0 || (i > 8 || j > 8) || j === 0) {
+				className += " text"
+				block.className = className;
+				if (i === 0) {
+					block.innerText = arr[j];
+				} else if (j === 0) {
+					flag = !flag;
+					block.innerText = arr2[i];
+				}
+				continue;
+			}
+
+			if (flag) className += " black";
+			else className += " white";
+
+			block.className = className;
+			block.dataset.address=arr[j]+arr2[i];
+			flag = !flag;
 		}
 	}
-	
-    for (var n = 10; n <=89; n+=10) {
-    	  sas-=1;
-			allblock[n].className="block text";
-			allblock[n].innerText=sas;
-			allblock[n-1].className="block white_border";
-		}
-		allblock[89].className="block white_border";	
-	for (var x = 90; x < 100; x++) {
-		allblock[x].className="block white_border";	
-		}	
-	}
+}
+
+function place_figure(fig_obj) {
+	let elem = document.createElement("img");
+	console.log(pawn_1.location);
+	let placement_div = document.querySelector('[data-address = ' + pawn_1.location + ']');
+	console.log(placement_div);
+	placement_div.appendChild(elem);
+
+	// todo здесь надо пошаманить, чтобы картинки корректно отображались, проверять в OpenServer надо
+	let url = window.location.href + fig_obj.background;
+	elem.src = url;
+
+	// записываем класс фигурки в класс img, чтобы в css размер задать ;)
+	elem.className = pawn_1.constructor.name;
+}
+
 
 Chess();
-var black_cell=document.querySelectorAll(".block black");
-var white_cell=document.querySelectorAll(".block white");
-console.log(black_cell);
-var themeButton = document.querySelector('.theme-button');
-themeButton.onclick=function(){
-	white_cell.classList.toggle("alt-white")
-	black_cell.classList.toggle("alt_black")
-}
-//пешки-------------------------------------
-pawn_1={
-	color:"white",
-	location:"B2"
-}
-pawn_2={
-	color:"white",
-	location:"C2"
-}
-pawn_3={
-	color:"white",
-	location:"D2"
-}
-pawn_4={
-	color:"white",
-	location:"E2"
-}
-pawn_5={
-	color:"white",
-	location:"F2"
-}
-pawn_6={
-	color:"white",
-	location:"G2"
-}
-pawn_7={
-	color:"white",
-	location:"H2"
-}
-pawn_8={
-	color:"white",
-	location:"A2"
-}
-//фигуры
-king={
-	color:"white",
-	location:"D1"
-}
-queen={
-	color:"white",
-	location:"E1"
-}
-rook_1={
-	color:"white",
-	location:"A1"
-}
-rook_2={
-	color:"white",
-	location:"H1"
-}
-horse_1={
-	color:"white",
-	location:"B1"
-}
-horse_2={
-	color:"white",
-	location:"G1"
-}
-slon_1={
-	color:"white",
-	location:"C1"
-}
-slon_2={
-	color:"white",
-	location:"F1"
-}
+
+// todo придумай как разместить сразу все пешки
+pawn_1 = new Pawn("B2", "white");
+place_figure(pawn_1);
+
